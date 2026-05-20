01d1bab35887c5926e0f92e57f5c5d1a6b40e12e
theothebault5-cell <theothebault5@gmail.com>
2026-05-20 20:53:35 +0200

    Update com_test.py

--- partie-1-raspberry-pi/com_test.py
+++ partie-1-raspberry-pi/com_test.py
@@ -1,1 +1,13 @@
+from client import MK3Client
+import time
 
+client = MK3Client("/dev/ttyUSB0")
+client.connect()
+
+while True:
+    client.request_data()
+    time.sleep(0.2)
+
+    raw = client.dev.read(64)
+    if raw:
+        print("RAW:", raw.hex())
