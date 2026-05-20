d805f59c40f0c09ba97e1578a46e812209a8d8d7
theothebault5-cell <theothebault5@gmail.com>
2026-05-20 20:54:37 +0200

    Update test_mk3.py

--- partie-1-raspberry-pi/test_mk3.py
+++ partie-1-raspberry-pi/test_mk3.py
@@ -1,1 +1,13 @@
+import serial
+import time
+
+ser = serial.Serial('/dev/ttyUSB0', 2400, timeout=1)
+ser.write(bytes.fromhex("555555555502ff4cb3"))
+time.sleep(0.1)
+ser.write(bytes.fromhex("04ff4101ffbc"))
+
+while True:
+    data = ser.read(64)
+    if data:
+        print(data.hex())
 
