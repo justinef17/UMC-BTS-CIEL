92caf616f7bcbd81e41b328d55886b3112316250
theothebault5-cell <theothebault5@gmail.com>
2026-05-21 09:19:07 +0200

    Update com.py

--- partie-1-raspberry-pi/com.py
+++ partie-1-raspberry-pi/com.py
@@ -1,13 +1,17 @@
-import serial
+from client import MK3Client
 import time
 
-ser = serial.Serial('/dev/ttyUSB0', 2400, timeout=1)
-ser.write(bytes.fromhex("555555555502ff4cb3"))
-time.sleep(0.1)
-ser.write(bytes.fromhex("04ff4101ffbc"))
+client = MK3Client()
+client.connect()
 
 while True:
-    data = ser.read(64)
-    if data:
-        print(data.hex())
+    client.request_data()
 
+    time.sleep(0.3)  
+
+#    for _ in range(10):
+    data = client.read()
+    if data:
+        print(data)
+
+    time.sleep(0.05)  
