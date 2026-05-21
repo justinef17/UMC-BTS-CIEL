73ce1a066cddfc2c97ed13fdcb3c3c5118eb136b
theothebault5-cell <theothebault5@gmail.com>
2026-05-21 09:18:03 +0200

    Update mk3.py

--- partie-1-raspberry-pi/mk3.py
+++ partie-1-raspberry-pi/mk3.py
@@ -1,4 +1,29 @@
 import serial
-ser = serial.Serial('/dev/ttyUSB0', 2400, timeout=1)
-print("OK connecte")
+import time
 
+class MK3Device:
+    def __init__(self, port="/dev/ttyUSB0", baudrate=2400):
+        self.port = port
+        self.baudrate = baudrate
+        self.ser = None
+
+    def open(self):
+        self.ser = serial.Serial(
+            port=self.port,
+            baudrate=self.baudrate,
+            bytesize=serial.EIGHTBITS,
+            parity=serial.PARITY_NONE,
+            stopbits=serial.STOPBITS_ONE,
+            timeout=0.2
+        )
+        time.sleep(0.1)
+
+    def write(self, data: bytes):
+        self.ser.write(data)
+
+    def read(self, size=64):
+        return self.ser.read(size)
+
+    def close(self):
+        if self.ser and self.ser.is_open:
+            self.ser.close()
